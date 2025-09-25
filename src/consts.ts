f27f99024a188607b09c9272fc748d8a2be1d763
eloypgweb <eloypg.web@gmail.com>
2025-09-25 13:09:02 +0200

    Added implements for all the project. Changed architecture and folder structure. Changed configurations.

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'JunStats';
-export const SITE_DESCRIPTION = 'Bienvenido a JunStats!';
+export const SITE_DESCRIPTION = 'Estadísticas y análisis del equipo de baloncesto junior. Seguimiento de jornadas, jugadas y rendimiento del equipo.';
