77ecf835101a87d55d4d8657b70c869dbc2c938a
eloypgweb <titoeloc@gmail.com>
2025-10-28 22:47:46 +0100

    VERCEL adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://junstats.vercel.app/',
+	// Usar salida en modo servidor y registrar el adaptador de Vercel
+	output: 'server',
+	adapter: vercel(),
 	integrations: [mdx(), sitemap()],
 });
